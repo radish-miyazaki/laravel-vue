44929d191768fa70fc41736f96e8592861fa80dc
radish-miyazaki <y.hidaka.kobe@gmail.com>
2020-12-11 10:41:04 +0900

    implement export csv to OrdesList

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -19,4 +19,37 @@ class OrderController extends Controller
     {
         return new OrderResource(Order::find($id));
     }
+
+    public function export()
+    {
+        $headers = [
+            "Content-type" => "type/csv",
+            "Content-Disposition" => "attachment; filename=orders.csv",
+            "Pragma" => "no-cache",
+            "Cache-Control" => "must-revalidate, post-check=0, pre-check=0",
+            "Expires" => "0"
+        ];
+
+        $callback = function() {
+            $orders = Order::all();
+            $file = fopen('php://output', 'w');
+
+            // HeaderRow
+            fputcsv($file, ['ID', 'Name', 'Email', 'Product Title', 'Price', 'Quantity']);
+
+            // Body
+            foreach ($orders as $order) {
+                fputcsv($file, [$order->id, $order->name, $order->email, '', '', '']);
+
+                foreach ($order->orderItems as $orderItem) {
+                    fputcsv($file, ['', '', '', $orderItem->product_title, $orderItem->price, $orderItem->quantity]);
+                }
+
+            }
+
+            fclose($file);
+        };
+
+        return \Response::stream($callback, 200, $headers);
+    }
 }
