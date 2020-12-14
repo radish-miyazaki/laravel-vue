16be3f8afe40c94ef83daf3436c7bfeaa3f82668
radish-miyazaki <y.hidaka.kobe@gmail.com>
2020-12-14 14:31:41 +0900

    implement card payment(stripe)

--- app/Http/Controllers/Checkout/OrderController.php
+++ app/Http/Controllers/Checkout/OrderController.php
@@ -6,6 +6,7 @@ use App\Models\Link;
 use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\Product;
+use Cartalyst\Stripe\Stripe;
 use Illuminate\Http\Request;
 
 class OrderController
@@ -13,7 +14,7 @@ class OrderController
     public function store(Request $request)
     {
         $link = Link::whereCode($request->input('code'))->first();
-        
+
         \DB::beginTransaction();
 
         $order = new Order();
@@ -32,6 +33,8 @@ class OrderController
 
         $order->save();
 
+        $lineItems = [];
+
         foreach ($request->input('items') as $item) {
             $product = Product::find($item['product_id']);
 
@@ -44,10 +47,32 @@ class OrderController
             $orderItem->admin_revenue = 0.9 * $product->price * $item['quantity'];
 
             $orderItem->save();
+
+            $lineItems[] = [
+                'name' => $product->title,
+                'description' => $product->description,
+                'images' => [
+                    $product->image,
+                ],
+                'amount' => 100 * $product->price,
+                'currency' => 'usd',
+                'quantity' => $orderItem->quantity,
+            ];
         }
+
+        $stripe = Stripe::make(env('STRIPE_SECRET'));
+        $source = $stripe->checkout()->sessions()->create([
+            'payment_method_types' => ['card'],
+            'line_items' => $lineItems,
+            'success' => env('CHECKOUT_URL') . '/success?source={CHECKOUT_SESSION_ID}',
+            'cancel_url' => env('CHECKOUT_URL') . '/error'
+        ]);
+
+        $order->transaction_id = $source['id'];
+        $order->save();
 
         \DB::commit();
 
-        return $order;
+        return $source;
     }
 }
